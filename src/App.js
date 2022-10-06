5a8796af6813bef185a03f9f3b7b8c0fe48b36ca
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-10-06 23:58:02 +0530

    feat: import modules and create blank scrollable app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ height: '700vh' }}></div>
+    </div>
+  );
+}
+
+export default App;
