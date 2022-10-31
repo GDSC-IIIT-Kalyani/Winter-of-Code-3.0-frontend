7008de4085b9df85de63822254c5b3540259e11f
Ayushi <ayushi0628@gmail.com>
2022-10-31 18:35:25 +0530

    add sponsors-section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavBar from './components/navBar/navBar.js';
 import Timeline from './components/timeline/timeline';
 import About from './components/about/about.js';
 import background from './assets/background.jpg';
+import Sponsor from './components/sponsors/sponsor.js';
+import asset from './assets/sponsors-bg-image.png';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <NavBar />
         <About />
         <Timeline />
+        <img className="background" src={asset} alt="background"></img>
+        <Sponsor/>
     </div>
   );
 }
