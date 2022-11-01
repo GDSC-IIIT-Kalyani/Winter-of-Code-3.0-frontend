f3b107ecdb586b823b7395bec3bab5bd44448b22
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-11-01 21:03:08 +0530

    feat: add hamburger menu for mobile view

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,39 +1,120 @@
-import React from "react";
-import './navBar.css';
+import React, { useState } from "react";
+import "./navBar.css";
 
 const NavBar = ({ navLinkColor }) => {
-    return (
-        <div className="header" id="#navbar">
-            <ul>
-                <li>
-                    <a href="/#About" style={{ color: navLinkColor }}>
-                        About
-                    </a>
-                </li>
-                <li>
-                    <a href="/projects" style={{ color: navLinkColor }}>
-                        Projects
-                    </a>
-                </li>
-                <li>
-                    <a href="/#timeline" style={{ color: navLinkColor }}>
-                        Timeline
-                    </a>
-                </li>
-                <li>
-                    <a href="/#sponsors" style={{ color: navLinkColor }}>
-                        Sponsors
-                    </a>
-                </li>
-                <li>
-                    <a href="/#our-team" style={{ color: navLinkColor }}>
-                        Our Team
-                    </a>
-                </li>
-            </ul>
-        </div>
+    const [isMobView, setIsMobView] = useState(false);
+    const [isCrossBurger, setIsCrossBurger] = useState(false);
 
-    )
-}
+    const handleHamburgerClick = (event) => {
+        setIsMobView(!isMobView);
+        setIsCrossBurger(!isCrossBurger);
+        const nav_links = document.querySelectorAll(".navBar_link-item");
+        nav_links.forEach((link, number, event) => {
+          if (link.style.animation) link.style.animation = "";
+          else
+            link.style.animation = `fade 0.5s ease forwards ${number / 7 + 0.2}s`;
+        });
+      };
+
+    return (
+    <nav
+      className={`navBar ${isScrollDown ? "navBar_scroll" : null}`}
+      onScroll={handleScrollDown}
+    >
+      <div className="navBar_container">
+        <ul className={`navBar_links ${isMobView ? "mob-view" : null}`}>
+          <li className="navBar_link-item home_comp">
+            <a
+              style={{ color: navLinkColor }}
+              href="/"
+              onClick={handleNavClick}
+            >
+              Home
+            </a>
+          </li>
+          <li className="navBar_link-item home_mob">
+            <a
+              style={{ color: navLinkColor }}
+              href="/"
+              onClick={handleNavClick}
+            >
+              Home
+            </a>
+          </li>
+          <li className="navBar_link-item">
+            <a
+              style={{ color: navLinkColor }}
+              href="/#About"
+              onClick={handleNavClick}
+            >
+              About
+            </a>
+          </li>
+          <li className="navBar_link-item">
+            <a
+              style={{ color: navLinkColor }}
+              href="/projects"
+              onClick={handleNavClick}
+            >
+              Projects
+            </a>
+          </li>
+          <li className="navBar_link-item">
+            <a
+              style={{ color: navLinkColor }}
+              href="/#timeline"
+              onClick={handleNavClick}
+            >
+              Timeline
+            </a>
+          </li>
+          <li className="navBar_link-item">
+            <a
+              style={{ color: navLinkColor }}
+              href="/#sponsors"
+              onClick={handleNavClick}
+            >
+              Sponsors
+            </a>
+          </li>
+
+
+          <li className="navBar_link-item">
+            <a
+              style={{ color: navLinkColor }}
+              href="/#developer"
+              onClick={handleNavClick}
+            >
+              Our Team
+            </a>
+          </li>
+
+          <li className="navBar_link-item">
+            <a
+              style={{ color: navLinkColor }}
+              href="/#faq"
+              onClick={handleNavClick}
+            >
+              FAQs
+            </a>
+          </li>
+
+        </ul>
+        <div className="hamburger_container">
+          <div
+            className={`navBar_hamburger ${
+              isCrossBurger ? "cross-burger" : null
+            }`}
+            onClick={handleHamburgerClick}
+          >
+            <div className="navBar_line1"></div>
+            <div className="navBar_line2"></div>
+            <div className="navBar_line3"></div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
