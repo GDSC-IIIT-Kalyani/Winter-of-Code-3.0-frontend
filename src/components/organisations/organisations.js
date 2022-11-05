bccfd889713993fdc2468464a863a152ab40f7b7
halyris <81964336+halyris@users.noreply.github.com>
2022-11-05 16:49:37 +0530

    added background

--- src/components/organisations/organisations.js
+++ src/components/organisations/organisations.js
@@ -8,39 +8,42 @@ import images from "./images.js";
 const Organisations = () => {
     return (
         <section id="organisations">
-            <div className="orgs-flex">
-                <div className="orgs-left">
-                    <h2 className="orgs-header">ORGANISATIONS</h2>
-                    <div className="orgs-image-container">
+            <img className="organisations-background" src={background} alt="background"></img>
+            <div className="organisations-content">
+                <div className="orgs-flex">
+                    <div className="orgs-left">
+                        <h2 className="orgs-header">ORGANISATIONS</h2>
+                        <div className="orgs-image-container">
 
-                        <div className="orgs-image-rows row-stretch">
-                            <img className="org-image" src={images["abc"]} alt=""></img>
-                            <img className="org-image" src={images["autodl"]} alt=""></img>
-                            <img className="org-image" src={images["hoppscotch"]} alt=""></img>
-                        </div>
+                            <div className="orgs-image-rows row-stretch">
+                                <img className="org-image" src={images["abc"]} alt="ABC organization logo"></img>
+                                <img className="org-image" src={images["autodl"]} alt="Auto-DL logo"></img>
+                                <img className="org-image" src={images["hoppscotch"]} alt="Hoppscotch logo"></img>
+                            </div>
 
-                        <div className="orgs-image-rows row-stagger">
-                            <img className="org-image" src={images["gitAInit"]} alt=""></img>
-                            <img className="org-image" src={images["llvm"]} alt=""></img>
-                            <img className="org-image" src={images["codeDigger"]} alt=""></img>
-                        </div>
+                            <div className="orgs-image-rows row-stagger">
+                                <img className="org-image" src={images["gitAInit"]} alt="Git init logo"></img>
+                                <img className="org-image" src={images["llvm"]} alt="LLVM logo"></img>
+                                <img className="org-image" src={images["codeDigger"]} alt="Code Digger logo"></img>
+                            </div>
 
-                        <div className="orgs-image-rows row-stretch">
-                            <img className="org-image" src={images["laayak"]} alt=""></img>
-                            <img className="org-image" src={images["gdsc"]} alt=""></img>
-                            <img className="org-image" src={images["dezenix"]} alt=""></img>
-                        </div>
+                            <div className="orgs-image-rows row-stretch">
+                                <img className="org-image" src={images["laayak"]} alt="Laayak logo"></img>
+                                <img className="org-image" src={images["gdsc"]} alt="GDSC logo"></img>
+                                <img className="org-image" src={images["dezenix"]} alt="Dezenix logo"></img>
+                            </div>
 
-                        <div className="orgs-image-rows row-stagger">
-                            <img className="org-image" src={images["mojaGlobal"]} alt=""></img>
-                            <img className="org-image" src={images["asyncAPI"]} alt=""></img>
+                            <div className="orgs-image-rows row-stagger">
+                                <img className="org-image" src={images["mojaGlobal"]} alt="Moja Global logo"></img>
+                                <img className="org-image" src={images["asyncAPI"]} alt="Async API logo"></img>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="orgs-right">
-                    <img className="orgs-whale-bg" src={whale_bg} alt="orgs-whale-background"></img>
-                    <img className="orgs-whale" src={whale} alt="orgs-whale"></img>
+                    <div className="orgs-right">
+                        <img className="orgs-whale-bg" src={whale_bg} alt="whale-background"></img>
+                        <img className="orgs-whale" src={whale} alt="whale"></img>
+                    </div>
                 </div>
             </div>
         </section>
