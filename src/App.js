9f6c2e118b72c3fe3c7edf7fe4b50fe2d58284a1
halyris <81964336+halyris@users.noreply.github.com>
2022-11-05 16:25:32 +0530

    Created 'Projects' section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/navBar/navBar.js';
 import Timeline from './components/timeline/timeline';
 import About from './components/about/about.js';
 import Organisations from './components/organisations/organisations';
+import Projects from './components/projects/projects';
 import background from './assets/background.jpg';
 import Sponsor from './components/sponsors/sponsor.js';
 import Footer from './components/footer/footer';
@@ -16,6 +17,7 @@ function App() {
         <About />
         <Timeline />
         <Organisations />
+        <Projects />
         <Sponsor/>
         <Faq />
         <Footer />
---
+++ src/components/projects/projects.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./projects.css"
+import image from "../../assets/projects_section_bg.png"
+
+const Projects = () => {
+    return (
+        <section id="projects">
+            <div className="projects-image-grid">
+                <img className="projects-image image-1" src={image} alt="projects"></img>
+                <img className="projects-image image-2" src={image} alt="projects"></img>
+                <img className="projects-image image-3" src={image} alt="projects"></img>
+                <img className="projects-image image-4" src={image} alt="projects"></img>
+                <img className="projects-image image-5" src={image} alt="projects"></img>
+                <img className="projects-image image-6" src={image} alt="projects"></img>
+                <img className="projects-image image-7" src={image} alt="projects"></img>
+                <img className="projects-image image-8" src={image} alt="projects"></img>
+                <img className="projects-image image-9" src={image} alt="projects"></img>
+            </div>
+            <div className="projects-textual-content">
+                <h2 className="projects-header">PROJECTS</h2>
+                <p className="projects-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <a href="#Projects" alt="Projects page">
+                    <button className="projects-button" type="button"><span>PROJECTS</span></button>
+                </a>
+            </div>
+        </section>
+    )
+}
+
+export default Projects;
