738d9fd19088fff93c0df32386c37901522124af
OindrilaDas27 <oindrila.das2708@gmail.com>
2022-11-07 00:36:14 +0530

    add hamburger menu

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,54 +1,63 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
 import "./navBar.css";
+import image from "../../assets/logo.png";
 
-const NavBar = ({ navLinkColor }) => {
-  const [isMobView, setIsMobView] = useState(false);
-  const [isCrossBurger, setIsCrossBurger] = useState(false);
-  const [isScrollDown, setIsScrollDown] = useState(false);
+function NavBar() {
+  const navRef = useRef();
 
-  const handleHamburgerClick = (event) => {
-    setIsMobView(!isMobView);
-    setIsCrossBurger(!isCrossBurger);
-    const nav_links = document.querySelectorAll(".navBar_link-item");
-    nav_links.forEach((link, number, event) => {
-      if (link.style.animation) link.style.animation = "";
-      else
-        link.style.animation = `fade 0.5s ease forwards ${number / 7 + 0.2}s`;
-    });
-    handleHamburgerClick(event);
-  };
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
+  // const [isMobView, setIsMobView] = useState(false);
+  // const [isCrossBurger, setIsCrossBurger] = useState(false);
+  // const [isScrollDown, setIsScrollDown] = useState(false);
 
-  const handleScrollDown = () => {
-    if (window.scrollY >= 80) {
-      setIsScrollDown(true);
-    } else {
-      setIsScrollDown(false);
-    }
-  };
+  // const handleHamburgerClick = (event) => {
+  //   setIsMobView(!isMobView);
+  //   setIsCrossBurger(!isCrossBurger);
+  //   const nav_links = document.querySelectorAll(".navBar_link-item");
+  //   nav_links.forEach((link, number, event) => {
+  //     if (link.style.animation) link.style.animation = "";
+  //     else
+  //       link.style.animation = `fade 0.5s ease forwards ${number / 7 + 0.2}s`;
+  //   });
+  //   handleHamburgerClick(event);
+  // };
 
-  const handleNavClick = (event) => {
-    handleHamburgerClick(event);
-  };
+  // const handleScrollDown = () => {
+  //   if (window.scrollY >= 80) {
+  //     setIsScrollDown(true);
+  //   } else {
+  //     setIsScrollDown(false);
+  //   }
+  // };
 
-  window.addEventListener("scroll", handleScrollDown);
+  // const handleNavClick = (event) => {
+  //   handleHamburgerClick(event);
+  // };
+
+  // window.addEventListener("scroll", handleScrollDown);
 
   return (
-    <nav
-      className={`navBar ${isScrollDown ? "navBar_scroll" : "navbar_top"}`}
-      onScroll={handleScrollDown}
+    <nav 
+      // className={`navBar ${isScrollDown ? "navBar_scroll" : "navbar_top"}`}
+      // onScroll={handleScrollDown}
     >
-      <div className="navBar_container">
+      <div className="responsive-logo"><img className="woc-logo" src={image} alt="woc-logo" /></div>
+
+      <div className="navBar_container" ref={navRef} >
         <div className="woc">WOC.</div>
-        <ul className={`navBar_links ${isMobView ? "mob-view" : "md-4"}`}>
+        <ul className="navBar_links">
           <li className="navBar_link-item home_comp">
           </li>
           <li className="navBar_link-item home_mob">
           </li>
           <li className="navBar_link-item">
             <a
-              style={{ color: navLinkColor }}
-              href="/#About"
-              onClick={handleNavClick}
+              // style={{ color: navLinkColor }}
+              // href="/#About"
+              // onClick={handleNavClick}
               className="navBar_link"
             >
               About
@@ -56,9 +65,9 @@ const NavBar = ({ navLinkColor }) => {
           </li>
           <li className="navBar_link-item">
             <a
-              style={{ color: navLinkColor }}
-              href="/projects"
-              onClick={handleNavClick}
+              // style={{ color: navLinkColor }}
+              // href="/projects"
+              // onClick={handleNavClick}
               className="navBar_link"
             >
               Projects
@@ -66,9 +75,9 @@ const NavBar = ({ navLinkColor }) => {
           </li>
           <li className="navBar_link-item">
             <a
-              style={{ color: navLinkColor }}
-              href="/#timeline"
-              onClick={handleNavClick}
+              // style={{ color: navLinkColor }}
+              // href="/#timeline"
+              // onClick={handleNavClick}
               className="navBar_link"
             >
               Timeline
@@ -76,9 +85,9 @@ const NavBar = ({ navLinkColor }) => {
           </li>
           <li className="navBar_link-item">
             <a
-              style={{ color: navLinkColor }}
-              href="/#sponsors"
-              onClick={handleNavClick}
+              // style={{ color: navLinkColor }}
+              // href="/#sponsors"
+              // onClick={handleNavClick}
               className="navBar_link"
             >
               Sponsors
@@ -87,9 +96,9 @@ const NavBar = ({ navLinkColor }) => {
 
           <li className="navBar_link-item">
             <a
-              style={{ color: navLinkColor }}
-              href="/#developer"
-              onClick={handleNavClick}
+              // style={{ color: navLinkColor }}
+              // href="/#developer"
+              // onClick={handleNavClick}
               className="navBar_link"
             >
               Our Team
@@ -98,9 +107,9 @@ const NavBar = ({ navLinkColor }) => {
 
           <li className="navBar_link-item">
             <a
-              style={{ color: navLinkColor }}
-              href="/#faq"
-              onClick={handleNavClick}
+              // style={{ color: navLinkColor }}
+              // href="/#faq"
+              // onClick={handleNavClick}
               className="navBar_link"
             >
               FAQs
@@ -108,9 +117,10 @@ const NavBar = ({ navLinkColor }) => {
           </li>
 
           <button className="navBar_apply-now"><a className="apply-now-link" href="www.google.com" target="_blank">APPLY NOW</a></button>
+          
         </ul>
-
-        <div className="hamburger_container">
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}><FaTimes /></button>
+        {/* <div className="hamburger_container">
           <div
             className={`navBar_hamburger ${
               isCrossBurger ? "cross-burger" : null
@@ -121,8 +131,11 @@ const NavBar = ({ navLinkColor }) => {
             <div className="navBar_line2"></div>
             <div className="navBar_line3"></div>
           </div>
-        </div>
+        </div> */}
       </div>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
     </nav>
   );
 };
