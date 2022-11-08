4946c7ad744fd243db2bd5ec2112eee41e51c8db
Suryanshomar7240 <suryu7240@gmail.com>
2022-11-09 00:12:16 +0530

    responsive navbar + nav bar

--- src/components/welcomePage/welcomePage.js
+++ src/components/welcomePage/welcomePage.js
@@ -3,7 +3,7 @@ import NavBar from "../navBar/navBar";
 import "./welcomePage.css";
 import background from "../../assets/bg images/WOC_Landing-page_back-ground.png";
 
-const icebergUpper = "https://firebasestorage.googleapis.com/v0/b/winter-of-code-3.appspot.com/o/iceberg_upper.png?alt=media&token=d2f89fe5-7e43-4c5e-88fc-08630b63156b";
+// const icebergUpper = "https://firebasestorage.googleapis.com/v0/b/winter-of-code-3.appspot.com/o/iceberg_upper.png?alt=media&token=d2f89fe5-7e43-4c5e-88fc-08630b63156b";
 
 export default function WelcomePage() {
   return (
