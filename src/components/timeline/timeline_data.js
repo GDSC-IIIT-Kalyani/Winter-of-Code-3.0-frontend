f10f61ae5a3043575d96477a978fd6e22f83e603
halyris <81964336+halyris@users.noreply.github.com>
2022-12-03 23:49:12 +0530

    updated timeline

--- src/components/timeline/timeline_data.js
+++ src/components/timeline/timeline_data.js
@@ -1,38 +1,38 @@
 const data=[
     {
-        data: "2022-11-15",
-        date: "15TH NOV- 25TH DEC'22",
+        data: "2022-11-30",
+        date: "30th Nov - 30th Dec'22",
         title: "Student Application Open",
         state: "active"
     },
     {
-        data: "2022-12-24",
-        date: "24TH Dec'22",
+        data: "2023-01-01",
+        date: "1st Dec'22",
         title: "Inaurgral Ceremony",
         state: "coming"
     },
     {
-        data: "2022-12-25",
-        date: "25TH DEC- 5TH JAN'23",
+        data: "2023-01-01",
+        date: "1st Jan - 10th Jan'23",
         title: "Proposal Writing session",
         state: "coming"
     },
     {
-        data: "2023-01-05",
-        date: "5TH-15th Jan'23",
-        title: "Community bonding phase",
+        data: "2023-01-10",
+        date: "10th Jan - 20th Jan'23",
+        title: "Community Bonding Phase",
         state: "coming"
     },
     {
-        data: "2023-01-15",
-        date: "15th Jan - 28th Feb'23",
-        title: "Coding phase",
+        data: "2023-01-20",
+        date: "20th Jan - 5th Mar'23",
+        title: "Coding Phase",
         state: "coming"
     },
     {
-        data: "2023-03-15",
-        date: "1st - 15th MAR'23",
-        title: "Student Application Open",
+        data: "2023-03-5",
+        date: "5th Mar - 15th Mar'23",
+        title: "Mentor Feedback Phase",
         state: "coming"
     },
 ];
