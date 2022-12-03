e3b3f777bcc567442ee3e9d342898536af64eb90
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-12-04 00:38:44 +0530

    ui:update timeline

--- src/components/timeline/timeline_data.js
+++ src/components/timeline/timeline_data.js
@@ -7,19 +7,19 @@ const data=[
     },
     {
         data: "2023-1-20",
-        date: "20TH JAN'23",
+        date: "5TH JAN'23",
         title: "Inaurgral Ceremony",
         state: "coming"
     },
     {
         data: "2022-12-25",
-        date: "25TH DEC- 5TH JAN'23",
+        date: "5TH JAN- 12TH JAN'23",
         title: "Proposal Writing session",
         state: "coming"
     },
     {
         data: "2023-01-05",
-        date: "5TH-15th Jan'23",
+        date: "12TH-20tH JAN'23",
         title: "Community bonding phase",
         state: "coming"
     },
@@ -31,8 +31,8 @@ const data=[
     },
     {
         data: "2023-03-15",
-        date: "1st - 15th MAR'23",
-        title: "Student Application Open",
+        date: "5TH - 15TH MAR'23",
+        title: "Mentor Feedback Phase",
         state: "coming"
     },
 ];
