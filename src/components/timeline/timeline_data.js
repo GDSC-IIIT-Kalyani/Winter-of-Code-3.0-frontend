8af82710bb2fdd987927a7ecb03c15f96e0b9ab9
halyris <81964336+halyris@users.noreply.github.com>
2022-12-04 01:02:56 +0530

    updated timeline

--- src/components/timeline/timeline_data.js
+++ src/components/timeline/timeline_data.js
@@ -1,32 +1,32 @@
 const data=[
     {
         data: "2022-12-10",
-        date: "10TH DEC'22- 5TH JAN'23",
+        date: "10TH DEC - 5TH JAN'23",
         title: "Student Application Open",
         state: "active"
     },
     {
         data: "2023-1-20",
         date: "5TH JAN'23",
-        title: "Inaurgral Ceremony",
+        title: "Inaugural Ceremony",
         state: "coming"
     },
     {
         data: "2022-12-25",
-        date: "5TH JAN- 12TH JAN'23",
+        date: "5TH JAN - 12TH JAN'23",
         title: "Proposal Writing session",
         state: "coming"
     },
     {
         data: "2023-01-05",
-        date: "12TH-20tH JAN'23",
-        title: "Community bonding phase",
+        date: "12TH - 20TH JAN'23",
+        title: "Community Bonding phase",
         state: "coming"
     },
     {
         data: "2023-01-15",
-        date: "20TH JAN - 5TH MARCH'23",
-        title: "Coding phase",
+        date: "20TH JAN - 5TH MAR'23",
+        title: "Coding Phase",
         state: "coming"
     },
     {
