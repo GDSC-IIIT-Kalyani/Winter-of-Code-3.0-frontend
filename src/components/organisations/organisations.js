569b4c8d8b86ff12044e273504e135b650b3a480
Arya <96682467+ARYA-RAY@users.noreply.github.com>
2022-12-13 22:34:52 +0530

    Update organisations.js
    fixed brochure link buttons

--- src/components/organisations/organisations.js
+++ src/components/organisations/organisations.js
@@ -29,8 +29,8 @@ const Organisations = () => {
                         <h2 className="orgs-header">ORGANISATIONS</h2>
                         <div className="buttn-container">
                             <h3 className="sponsor-dsc">Our love for Open Source drove us to impact the community through Winter of Code 3.0. We would love more collaborators for our initiative </h3>
-                            <a href="https://drive.google.com/file/d/1MfoVIZaS_Wd53HmsxJ0ziKZRBEjbjA6I/view" alt="Organizations Brochure" target="_blank" rel="noopener noreferrer">
-                                <button className="brochure-buttn" type="button"><span>ORGANISATION BROCHURE</span></button>
+                            <a href="https://drive.google.com/file/d/1MfoVIZaS_Wd53HmsxJ0ziKZRBEjbjA6I/view" alt="Organizations Brochure" target="_blank" rel="noopener noreferrer" className="org-link">
+                                ORGANISATION BROCHURE
                             </a>
                         </div>
                         <div className="orgs-image-container">
