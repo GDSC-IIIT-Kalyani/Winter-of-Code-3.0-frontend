4c0dc915d3067807fec2fa2dd08b5e394cdab0da
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-12-30 00:51:36 +0530

    feat: enable microsoft analytics

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Projectspage from "./pages/allProjects/ProjectsPage.js";
+import { clarity } from "clarity-js";
 
 function App() {
+  clarity();
+  clarity("track", "pageview");
+  clarity("track", "click");
+  clarity("track", "form");
+  clarity("track", "scroll");
+  clarity("track", "input");
+  clarity("track", "change");
+  clarity("track", "error");
+  clarity("track", "custom", "customEvent");
   return (
     <Router>
       <Routes>
@@ -13,4 +23,7 @@ function App() {
     </Router>
   );
 }
+
+clarity("track", "click");
+
 export default App;
