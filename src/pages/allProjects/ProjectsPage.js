87db240c1d0b9554d081ddec71afb04fbc814e51
Arya <aryapratimray@gmail.com>
2023-01-04 17:27:02 +0530

    terraforge3d

--- src/pages/allProjects/ProjectsPage.js
+++ src/pages/allProjects/ProjectsPage.js
@@ -110,6 +110,7 @@ const ProjectsPage = () => {
             <option value="alpha">Alpha.Dev</option>
             <option value="SamagraX">SamagraX</option>
             <option value="polyaxon">Polyaxon</option>
+            <option value="terraforge">TerraForge3D</option>
             <option value="gdsc">GDSC</option>
           </select>
         </form>
