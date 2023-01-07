03a4ba82f1a1b5eb7edf019abbee50daa4747423
Suryanshomar7240 <suryu7240@gmail.com>
2023-01-07 17:13:26 +0530

    first page buttons

--- src/components/timeline/timeline_data.js
+++ src/components/timeline/timeline_data.js
@@ -13,19 +13,19 @@ const data=[
     },
     {
         data: "2022-12-25",
-        date: "5TH JAN - 20TH JAN'23",
+        date: "12TH JAN - 20TH JAN'23",
         title: "Proposal Writing session",
         state: "coming"
     },
     {
         data: "2023-01-05",
-        date: "12TH - 20TH JAN'23",
+        date: "20TH - 25TH JAN'23",
         title: "Community Bonding phase",
         state: "coming"
     },
     {
         data: "2023-01-15",
-        date: "20TH JAN - 5TH MAR'23",
+        date: "25TH JAN - 5TH MAR'23",
         title: "Coding Phase",
         state: "coming"
     },
