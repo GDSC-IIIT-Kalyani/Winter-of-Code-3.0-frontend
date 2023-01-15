8f136b79f8ae6de03a9616a9f194876cb9d71567
gauravshukla2203 <gaurav.shukla.gs48@gmail.com>
2023-01-16 00:45:53 +0530

    added apply button in mobile view

--- src/components/welcomePage/welcomePageMobile.js
+++ src/components/welcomePage/welcomePageMobile.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../navBar/navBar";
 import "./welcomePageMobile.css";
 import Aboutus from "../about/about";
+import "../navBar/navBar.css";
 
 export default function WelcomePage() {
   return (
@@ -10,8 +11,13 @@ export default function WelcomePage() {
         <NavBar />
       </div>
       <div style={{paddingTop:'60vw'}}><Aboutus />
-      <div className = "applyButton">
-                  <a href="https://winter-of-code.devfolio.co/">Apply with Devfolio</a></div></div>
+      
+            <div className="apply-button"
+            data-hackathon-slug="winter-of-code"
+            data-button-theme="light"></div>
+
+            
+      </div>
     </div>
   );
 }
