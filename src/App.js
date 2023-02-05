4745e64161e5ef4235d9ca04cee3e1d2dca9a3ac
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-02-05 13:38:48 +0530

    Adding lazy loading feature

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Projectspage from "./pages/allProjects/ProjectsPage.js";
-import NavBar from "./components/navBar/navBar";
-import About from "./components/about/about";
-import Timeline from "./components/timeline/timeline";
-import Organisations from "./components/organisations/organisations";
-import Sponsor from "./components/sponsors/sponsor";
-import Faq from "./components/faq/faq";
+import Loader from "./components/loader/loader";
+
+const About = lazy(() => import("./components/about/about"));
+const Faq = lazy(() => import("./components/faq/faq"));
+const NavBar = lazy(() => import("./components/navBar/navBar"));
+const Organisations = lazy(() => import("./components/organisations/organisations"));
+const Sponsor = lazy(() => import("./components/sponsors/sponsor"));
+const Timeline = lazy(() => import("./components/timeline/timeline"));
+const Projectspage = lazy(() => import("./pages/allProjects/ProjectsPage.js"));
+const Home = lazy(() => import("./pages/Home/Home"));
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route
-          exact
-          path="/projects"
-          element={<Projectspage pageTop={true} />}
-        />
-        <Route exact path="/about" element={<About pageTop={true} />} />
-        <Route exact path="/timeline" element={<Timeline pageTop={true} />} />
-        <Route
-          exact
-          path="/organisations"
-          element={<Organisations pageTop={true} />}
-        />
-        <Route exact path="/sponsors" element={<Sponsor pageTop={true} />} />
-        <Route exact path="/faqs" element={<Faq pageTop={true} />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <NavBar />
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route
+            exact
+            path="/projects"
+            element={<Projectspage pageTop={true} />}
+          />
+          <Route exact path="/about" element={<About pageTop={true} />} />
+          <Route exact path="/timeline" element={<Timeline pageTop={true} />} />
+          <Route
+            exact
+            path="/organisations"
+            element={<Organisations pageTop={true} />}
+          />
+          <Route exact path="/sponsors" element={<Sponsor pageTop={true} />} />
+          <Route exact path="/faqs" element={<Faq pageTop={true} />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
